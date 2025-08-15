4526d25b8de1e6715a77ebfb7c13c74b12c743e1
Bashar Astifan <accounts@astifan.online>
2025-08-15 11:12:57 +0400

    ImMobile 1.2.1
    Small fix for 8.1 files

--- development/ExtensionsExamples/Win8.1/BingWallpaper/ImmExtenTemplate8.1.Shared/Others/dllmain.cpp
+++ development/ExtensionsExamples/Win8.1/BingWallpaper/ImmExtenTemplate8.1.Shared/Others/dllmain.cpp
@@ -3,7 +3,7 @@ Date: 2025-Present
 
 Bashar Astifan (https://github.com/basharast):
 ImMobile Extension
-Legacy support as tested on 10240
+Legacy support as tested on 8.1 (6.3.9651.0)
 */
 
 #include "pch.h"
